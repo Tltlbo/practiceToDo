917982e40ff02e21ff598284a594908f3d3f7b91
Tltlbo <js1436kt@gmail.com>
2023-02-10 02:33:39 +0900

    Initial Commit

---
+++ practiceToDo/practiceToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceToDoApp.swift
+//  practiceToDo
+//
+//  Created by 박진성 on 2023/02/10.
+//
+
+import SwiftUI
+
+@main
+struct practiceToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
